9371e1d429c7c0ea121a1473e27ca91eced33216
mitu <catvinchy@gmail.com>
2016-05-04 16:23:29 +0600

    with primary key

--- app/src/main/java/com/example/mitu/realcontactbook/AddContact.java
+++ app/src/main/java/com/example/mitu/realcontactbook/AddContact.java
@@ -2,8 +2,10 @@ package com.example.mitu.realcontactbook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,21 +14,21 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import io.realm.Realm;
-import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
 
 public class AddContact extends AppCompatActivity {
     EditText etName, etPhone;
     ContactModel contactModel;
     Button addbutton,button;
-    public  Realm myRealm;
+    private   Realm myRealm;
     ListView lvContactList;
     public  RealmResults<ContactModel> results1;
     LinearLayout linearLayout;
     private ContactAdapter contactAdapter;
 
 
-    int _id;
+    public int _id=0;
+    long position_forUpdate= -1 ;
     TextView tv;
     Button del;
     @Override
@@ -41,9 +43,10 @@ public class AddContact extends AppCompatActivity {
         linearLayout = (LinearLayout) findViewById(R.id.layoutaddcontact);
         linearLayout.setVisibility(View.GONE);
 
-        RealmConfiguration realmConfig = new RealmConfiguration.Builder(this).build();
+//        RealmConfiguration realmConfig = new RealmConfiguration.Builder(this).build();
 
-        myRealm = Realm.getInstance(realmConfig);
+//        myRealm = Realm.getInstance(realmConfig);
+            myRealm = Realm.getDefaultInstance();
 
         //myRealm.beginTransaction();
         contactModel = new ContactModel();
@@ -51,36 +54,72 @@ public class AddContact extends AppCompatActivity {
         ///Query
         results1 = myRealm.where(ContactModel.class).findAll();
 
-        /*for(ContactModel c:results1) {
-            Log.d("results1", c.getmName() + c.getmPhone() + " ");
-        }*/
+        for(ContactModel c:results1) {
+            Log.d("results1", c.getmName() + c.getmPhone() + " "+c.getmId());
+        }
         contactAdapter = new ContactAdapter();
 
         lvContactList.setAdapter(contactAdapter);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        lvContactList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public void onClick(View v) {
-                String name = etName.getText().toString();
-                String phone = etPhone.getText().toString();
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                _id = -1;
+                position_forUpdate = id+1;
+                String name = results1.get(position).getmName();
+                String phone = results1.get(position).getmPhone();
+                addbutton.setVisibility(view.GONE);
+                linearLayout.setVisibility(view.VISIBLE);
+                etName.setText(name+"");
+                etPhone.setText(phone+"");
 
-                myRealm.beginTransaction();
-                contactModel.setmName(name);
-                contactModel.setmPhone(phone);
-                myRealm.copyToRealm(contactModel);
-                myRealm.commitTransaction();
-
-
-                linearLayout.setVisibility(v.GONE);
-                addbutton.setVisibility(v.VISIBLE);
-                results1 = myRealm.where(ContactModel.class).findAll();
-                contactAdapter = new ContactAdapter();
-
-                lvContactList.setAdapter(contactAdapter);
-
+               // addContact(view);
+                Log.d("Position ",position+"");
+                Log.d("id ",id+"");
+                return true;
             }
         });
 
+    }
 
+    public  void addContact(View v){
+
+
+
+        long id1;
+
+        if (_id > -1) {
+            id1 = results1.size() == 0 ? 1 : results1.size() + 1;
+            String name = etName.getText().toString();
+            String phone = etPhone.getText().toString();
+            myRealm.beginTransaction();
+            contactModel.setmName(name);
+            contactModel.setmPhone(phone);
+            contactModel.setmId( id1 );
+            myRealm.copyToRealm(contactModel);
+
+        }else if (_id == -1){
+
+
+            myRealm.beginTransaction();
+            contactModel.setmName(etName.getText().toString());
+            contactModel.setmPhone(etPhone.getText().toString());
+            contactModel.setmId(position_forUpdate);
+            myRealm.copyToRealmOrUpdate(contactModel);
+            position_forUpdate = -1;
+           // Log.d("Update","_id "+_id+"Previous "+getname+" ,"+getPhone+","+id2+" Now :"+contactModel.getmName()+","+contactModel.getmPhone()+","+contactModel.getmId());
+            _id = 0;
+        }
+       // myRealm.copyToRealmOrUpdate(contactModel);
+        myRealm.commitTransaction();
+
+
+        linearLayout.setVisibility(v.GONE);
+        addbutton.setVisibility(v.VISIBLE);
+        results1 = myRealm.where(ContactModel.class).findAll();
+        contactAdapter = new ContactAdapter();
+
+        lvContactList.setAdapter(contactAdapter);
 
     }
 
@@ -119,8 +158,8 @@ public class AddContact extends AppCompatActivity {
             TextView tvPhone = (TextView) convertView.findViewById(R.id.textView2);
 
             ContactModel ch = (ContactModel) getItem(position);
-            tvName.setText(ch.mName);
-            tvPhone.setText(ch.mPhone);
+            tvName.setText(ch.getmName());
+            tvPhone.setText(ch.getmPhone());
 
             return convertView;
         }
--- app/src/main/java/com/example/mitu/realcontactbook/ContactModel.java
+++ app/src/main/java/com/example/mitu/realcontactbook/ContactModel.java
@@ -1,23 +1,24 @@
 package com.example.mitu.realcontactbook;
 
 import io.realm.RealmObject;
-import io.realm.annotations.Required;
+import io.realm.annotations.PrimaryKey;
 
 /**
  * Created by mitu on 4/26/16.
  */
 public class ContactModel extends RealmObject {
 
-    private int mId;
-    @Required
-    public String mName;
-    @Required
-    public String mPhone;
+    @PrimaryKey
+    private long mId;
+    private String mName;
+
+    private String mPhone;
 
     public ContactModel() {
+
     }
 
-    public ContactModel(int mId, String mName, String mPhone) {
+    public ContactModel(long mId, String mName, String mPhone) {
         this.mId = mId;
         this.mName = mName;
         this.mPhone = mPhone;
@@ -28,8 +29,12 @@ public class ContactModel extends RealmObject {
         this.mPhone = mPhone;
     }
 
-    public void setmId(int mId) {
+    public void setmId(long mId) {
         this.mId = mId;
+    }
+
+    public long getmId() {
+        return mId;
     }
 
     public String getmName() {
